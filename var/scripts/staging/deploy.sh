fdea9a056814a9d7d14b1a44f89b89f0fef661c6
Khanh Nguyen <khanh@propolife.co.jp>
2021-12-31 14:17:31 +0700

    Update source: 20211231_1417

--- var/scripts/staging/deploy.sh
+++ var/scripts/staging/deploy.sh
@@ -14,7 +14,7 @@ DIST_PATH=dist
 FROM_PATH=$LOCAL_VUE_SRC/$DIST_PATH
 TO_PATH=$STG_RCF_SRC/$DIST_PATH
 
-# echo "rsync $OPTIONS --delete --exclude-from=$EXCLUDE_SRC_PUSH -e \"ssh -i $KEY_PATH\" $FROM_PATH/ $STG_EC2_ADDRESS:$TO_PATH/"
+# echo "rsync $OPTIONS --delete --exclude-from=$EXCLUDE_SRC_PUSH -e \"ssh -i $STG_EC2_KEY\" $FROM_PATH/ $STG_EC2_ADDRESS:$TO_PATH/"
 rsync $OPTIONS --delete --exclude-from=$EXCLUDE_SRC_PUSH -e "ssh -i $STG_EC2_KEY" $FROM_PATH/ $STG_EC2_ADDRESS:$TO_PATH/
 
 printf "\n\n${0##*\/}: Completed at http://vue-dev.rimawaru.com/ ...\n"
