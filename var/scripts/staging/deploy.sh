8069c763d9d3dd611410d8b1c5bc239652f23fc1
Khanh Nguyen <khanh@propolife.co.jp>
2021-12-31 14:06:00 +0700

    Update source: 20211231_1405

--- var/scripts/staging/deploy.sh
+++ var/scripts/staging/deploy.sh
@@ -2,7 +2,12 @@
 source ./var/.env
 
 yarn --cwd $(pwd)/base-vue/ install
-yarn --cwd $(pwd)/base-vue/ build
+
+
+if [ ! -z "$1" ] && [ "$1" == "--build" ]
+then
+    yarn --cwd $(pwd)/base-vue/ build
+fi
 
 # VUE SOURCES
 DIST_PATH=dist
@@ -10,6 +15,6 @@ FROM_PATH=$LOCAL_VUE_SRC/$DIST_PATH
 TO_PATH=$STG_RCF_SRC/$DIST_PATH
 
 # echo "rsync $OPTIONS --delete --exclude-from=$EXCLUDE_SRC_PUSH -e \"ssh -i $KEY_PATH\" $FROM_PATH/ $STG_EC2_ADDRESS:$TO_PATH/"
-rsync $OPTIONS --delete --exclude-from=$EXCLUDE_SRC_PUSH -e "ssh -i $KEY_PATH" $FROM_PATH/ $STG_EC2_ADDRESS:$TO_PATH/
+rsync $OPTIONS --delete --exclude-from=$EXCLUDE_SRC_PUSH -e "ssh -i $STG_EC2_KEY" $FROM_PATH/ $STG_EC2_ADDRESS:$TO_PATH/
 
 printf "\n\n${0##*\/}: Completed at http://vue-dev.rimawaru.com/ ...\n"
