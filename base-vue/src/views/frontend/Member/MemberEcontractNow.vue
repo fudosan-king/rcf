8e9171d940e1570eaf8b734bb4b2e98558df3b8f
Anh Thi <anhthi92@propolife.co.jp>
2022-01-27 15:20:43 +0700

    不特法（リマワル|rimawaru）_サービス新規構築_FE（サービス本体＆マイページ）

---
+++ base-vue/src/views/frontend/Member/MemberEcontractNow.vue
@@ -0,0 +1,85 @@
+<template>
+    <div>
+        <div class="main-content bg-green-15">
+           <section>
+               <div class="container">
+                    <from action="" method="">
+                        <div class="member e-contracts">
+                            <div class="title-page">
+                                <h1>お申込みの確認</h1>
+                                <p>不動産特定共同事業法に基づく匿名組合型の不動産特定共同事業契約の内容について、<br>契約成立時書面をご確認下さい。</p>
+                            </div>
+                            <div class="box">
+                                <div class="document">
+                                    <div class="document_pdf">
+                                        <pdf 
+                                        v-for="i in numPages"
+                                        :key="i"
+                                        :src="src"
+                                        :page="i"
+                                        ></pdf>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="confirm">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" disabled>
+                                    <label class="form-check-label" for="flexCheckDefault">
+                                        契約成立前交付書面を確認し、内容を理解しました。
+                                    </label>
+                                </div>
+                                <p>契約成立時書面PDFファイルが登録メールアドレス宛に通知されますのでご確認下さい。</p>
+                                <p>契約成立後はマイページにて契約成立時書面PDFファイルをご確認頂けます。</p>
+                            </div>
+                            <div class="text-center">
+                                <button type="submit"
+                                    class="btn btn-form-action bg-cl-green text-white">
+                                    <span>契約を締結する</span></button>
+                            </div>
+                           
+                        </div>
+                    </from>
+               </div>
+           </section>
+        </div>
+    </div>
+</template>
+
+<script>
+import pdf from 'vue-pdf'
+var loadingTask = pdf.createLoadingTask('/assets/pdf/sample-default.pdf');
+
+export default {
+    components: {
+        pdf
+    },
+    data() {
+        return {
+            src: loadingTask,
+            numPages: undefined,
+        }
+    },
+    mounted() {
+
+        this.src.promise.then(pdf => {
+            this.numPages = pdf.numPages;
+        });
+    },
+    updated(){
+        const documentPDF = document.querySelector('.document_pdf');   
+        const input = document.querySelector('.form-check input');
+        documentPDF.addEventListener("scroll",() => {
+            if( documentPDF.scrollTop + documentPDF.offsetHeight >= documentPDF.scrollHeight ){
+                input.disabled = false;
+            }
+        });
+    },
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+
+
+</style>
