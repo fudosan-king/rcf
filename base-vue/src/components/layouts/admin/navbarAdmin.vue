4e4fea5371daf3e071a7764f045c0608dca18d85
ntvan711 <vannt711@gmail.com>
2022-03-07 08:23:11 +0700

    Update source

--- base-vue/src/components/layouts/admin/navbarAdmin.vue
+++ base-vue/src/components/layouts/admin/navbarAdmin.vue
@@ -7,9 +7,6 @@
                         <img src="../../../assets/svgs/logo/logo.svg" class="logo-1"
                              alt="logo">
                     </router-link>
-                    <!--                    <router-link :to="{name: 'homeAdmin'}"><img src="../../../assets/svgs/logo/logo.svg" class="dark-logo-1"-->
-                    <!--                                                           alt="logo">-->
-                    <!--                    </router-link>-->
                 </div>
                 <div class="app-sidebar__toggle" data-toggle="sidebar">
                     <a class="open-toggle"><i class="header-icon fe fe-align-left"></i></a>
@@ -17,25 +14,28 @@
                 </div>
             </div>
             <div class="main-header-right">
-                <div class="nav nav-item  navbar-nav-right ml-auto">
-                    <div class="dropdown main-profile-menu nav nav-item nav-link">
-                        <a class="profile-user d-flex" href=""><img :src="avatar"></a>
-                        <div class="dropdown-menu">
-                            <div class="main-header-profile bg-primary p-3">
-                                <div class="d-flex wd-100p">
-                                    <div class="main-img-user text-center">
-                                        <img :src="avatar" alt=""></div>
-                                    <div class="m__left--2 my-auto">
-                                        <h6>{{
-                                                userInfo && (userInfo.profile && userInfo.profile.full_name || userInfo.first_name + userInfo.last_name)
-                                            }} </h6>
-                                        <span> {{ userInfo && (userInfo.role && userInfo.role.name) }}</span>
-                                    </div>
+                <div class="dropdown main-profile-menu" id="main-profile-menu"
+                     v-click-out="() => closeDropdown('main-profile-menu')">
+                    <a class="profile-user d-flex" @click="toggleDropdown('main-profile-menu')">
+                        <img :src="avatar">
+                    </a>
+                    <div class="dropdown-menu">
+                        <div class="main-header-profile background__green p-3">
+                            <div class="d-flex wd-100p">
+                                <div class="main-img-user text-center">
+                                    <img :src="avatar" alt=""></div>
+                                <div class="m__left--2 my-auto">
+                                    <h6>{{
+                                            userInfo && (userInfo.profile && userInfo.profile.full_name || userInfo.first_name + userInfo.last_name)
+                                        }} </h6>
+                                    <span> {{ userInfo && (userInfo.role && userInfo.role.name) }}</span>
                                 </div>
                             </div>
-                            <a class="dropdown-item" @click="profile"><i class="bx bx-user-circle"></i>Profile</a>
-                            <a class="dropdown-item" @click="logout()"><i class="bx bx-log-out"></i> Sign Out</a>
                         </div>
+                        <router-link :to="{name: 'profile.admin'}" class="dropdown-item">
+                            <i class="bx bx-user-circle"></i>Profile
+                        </router-link>
+                        <a class="dropdown-item" @click="logout()"><i class="bx bx-log-out"></i> Sign Out</a>
                     </div>
                 </div>
             </div>
@@ -45,10 +45,12 @@
 <script>
 
 import {mapGetters} from "vuex";
+import common from "@/mixins/common";
 import {ACTION_LOGOUT} from "@/stores/auth/actions";
 
 export default {
     name: "navbarAdmin",
+    mixins: [common],
     methods: {
         async logout() {
             await this.$store.dispatch(ACTION_LOGOUT).then(() => {
--- base-vue/src/views/backend/profile/Profile.vue
+++ base-vue/src/views/backend/profile/Profile.vue
@@ -28,11 +28,11 @@
                                                 <img src="../../../assets/images/camera-white.png">
                                             </figcaption>
                                         </figure>
-                                        <span class="error pt-1"
+                                        <span class="is-error pt-1"
                                               v-if="!vuelidate.profile.avatar.fileSizeValidation && vuelidate.$dirty">
                                             {{ $t('profile.messages.validation.avatar.file_size') }}
                                         </span>
-                                        <span class="error pt-1"
+                                        <span class="is-error pt-1"
                                               v-if="!vuelidate.profile.avatar.extension && vuelidate.$dirty">
                                             {{ $t('profile.messages.validation.avatar.format') }}
                                         </span>
@@ -43,22 +43,212 @@
                         <div class="row">
                             <div class="col-lg-3">
                                 <div class="form-group mg-b-0">
-                                    <label class="">{{ $t('pages.users.name.label') }}</label>
+                                    <label class="">{{ $t('pages.profile.input_field.username.label') }}</label>
                                     <i class="required-note"
                                        v-tooltip.right="{ customClass: 'tooltip-danger' }"
                                        :title="$t('tooltip.required')"
                                     >*</i>
                                 </div>
                             </div>
-                            <div class="col-lg-6 mg-t-20 mg-lg-t-0">
+                            <div class="col-lg-7 mg-t-20 mg-lg-t-0">
                                 <div class="form-group has-danger mg-b-0">
-                                    <input class="form-control" :placeholder="$t('pages.users.name.placeholder')"
+                                    <input class="form-control"
+                                           :placeholder="$t('pages.profile.input_field.username.placeholder')"
                                            v-model.trim="profile.username"
-                                           disabled
                                            type="text">
                                 </div>
                             </div>
                         </div>
+                        <div class="row">
+                            <div class="col-lg-3">
+                                <div class="form-group mg-b-0">
+                                    <label class="">{{ $t('pages.profile.input_field.full_name.label') }}</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-7 mg-t-20 mg-lg-t-0">
+                                <div class="form-group has-danger mg-b-0">
+                                    <input class="form-control"
+                                           :placeholder="$t('pages.profile.input_field.full_name.placeholder')"
+                                           v-model.trim="profile.full_name"
+                                           type="text">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-lg-3">
+                                <div class="form-group mg-b-0">
+                                    <label class="">{{ $t('pages.profile.input_field.katakana_name.label') }}</label>
+                                    <i class="required-note"
+                                       v-tooltip.right="{ customClass: 'tooltip-danger' }"
+                                       :title="$t('tooltip.required')"
+                                    >*</i>
+                                </div>
+                            </div>
+                            <div class="col-lg-7 mg-t-20 mg-lg-t-0">
+                                <div class="form-group has-danger mg-b-0">
+                                    <input class="form-control"
+                                           :placeholder="$t('pages.profile.input_field.katakana_name.placeholder')"
+                                           v-model.trim="profile.katakana_name"
+                                           type="text">
+                                    <span class="is-error pt-1"
+                                          v-if="error_server.katakana_name && error_server.katakana_name.length">
+                                        {{ error_server.katakana_name[0] }}
+                                    </span>
+                                    <template v-else-if="vuelidate.$dirty">
+                                        <span class="is-error pt-1" v-if="!vuelidate.profile.katakana_name.required">
+                                            {{ $t('profile.messages.validation.email_required') }}
+                                        </span>
+                                        <span class="is-error pt-1"
+                                              v-else-if="!vuelidate.profile.katakana_name.katakana()">
+                                            {{ $t('profile.messages.validation.email_invalid') }}
+                                        </span>
+                                    </template>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-3">
+                                <div class="form-group mg-b-0">
+                                    <label class="">{{ $t('pages.profile.input_field.email.label') }}</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-7 mg-t-20 mg-lg-t-0">
+                                <div class="form-group has-danger mg-b-0">
+                                    <input class="form-control"
+                                           :placeholder="$t('pages.profile.input_field.email.placeholder')"
+                                           v-model.trim="profile.email"
+                                           type="text">
+                                    <span class="is-error pt-1" v-if="error_server.email && error_server.email.length">
+                                        {{ error_server.email[0] }}
+                                    </span>
+                                    <template v-else-if="vuelidate.$dirty">
+                                        <span class="is-error pt-1" v-if="!vuelidate.profile.email.required">
+                                            {{ $t('profile.messages.validation.email_required') }}
+                                        </span>
+                                        <span class="is-error pt-1" v-else-if="!vuelidate.profile.email.email">
+                                            {{ $t('profile.messages.validation.email_invalid') }}
+                                        </span>
+                                    </template>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-3">
+                                <div class="form-group mg-b-0">
+                                    <label class="">{{ $t('pages.profile.input_field.phone.label') }}</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-7 mg-t-20 mg-lg-t-0">
+                                <div class="form-group has-danger mg-b-0">
+                                    <input class="form-control"
+                                           :placeholder="$t('pages.profile.input_field.phone.placeholder')"
+                                           v-model.trim="profile.phone"
+                                           type="text">
+                                    <span class="is-error pt-1" v-if="error_server.phone && error_server.phone.length">
+                                        {{ error_server.phone[0] }}
+                                    </span>
+                                    <template v-else-if="vuelidate.$dirty">
+                                        <span class="is-error pt-1" v-if="!vuelidate.profile.katakana_name.required">
+                                            {{ $t('profile.messages.validation.email_required') }}
+                                        </span>
+                                    </template>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-3">
+                                <div class="form-group mg-b-0">
+                                    <label class="">{{ $t('pages.profile.input_field.password.label') }}</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-7">
+                                <div class="form-group has-danger mg-b-0">
+                                    <input class="form-control"
+                                           :placeholder="$t('pages.profile.input_field.password.placeholder')"
+                                           v-model.trim="profile.password"
+                                           type="password">
+                                    <span class="is-error pt-1"
+                                          v-if="error_server.password && error_server.password.length">
+                                        {{ error_server.password[0] }}
+                                    </span>
+                                    <template v-else-if="vuelidate.$dirty">
+                                        <span class="is-error pt-1" v-if="!vuelidate.profile.password.required">
+                                            {{ $t('profile.messages.validation.password_required') }}
+                                        </span>
+                                        <span class="is-error pt-1" v-else-if="!vuelidate.profile.password.minLength">
+                                            {{ $t('profile.messages.validation.password_minLength') }}
+                                        </span>
+                                        <span class="is-error pt-1" v-else-if="!vuelidate.profile.password.maxLength">
+                                            {{ $t('profile.messages.validation.password_maxLength') }}
+                                        </span>
+                                    </template>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-3">
+                                <div class="form-group mg-b-0">
+                                    <label class="">{{ $t('pages.profile.input_field.new_password.label') }}</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-7">
+                                <div class="form-group has-danger mg-b-0">
+                                    <input class="form-control"
+                                           :placeholder="$t('pages.profile.input_field.new_password.placeholder')"
+                                           v-model.trim="profile.new_password"
+                                           type="password">
+                                    <span class="is-error pt-1"
+                                          v-if="error_server.new_password && error_server.new_password.length">
+                                        {{ error_server.new_password[0] }}
+                                    </span>
+                                    <template v-else-if="vuelidate.$dirty">
+                                        <span class="is-error pt-1" v-if="!vuelidate.profile.new_password.required">
+                                            {{ $t('profile.messages.validation.new_password_required') }}
+                                        </span>
+                                        <span class="is-error pt-1"
+                                              v-else-if="!vuelidate.profile.new_password.minLength">
+                                            {{ $t('profile.messages.validation.new_password_minLength') }}
+                                        </span>
+                                        <span class="is-error pt-1"
+                                              v-else-if="!vuelidate.profile.new_password.maxLength">
+                                            {{ $t('profile.messages.validation.new_password_maxLength') }}
+                                        </span>
+                                    </template>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-lg-3">
+                                <div class="form-group mg-b-0">
+                                    <label class="">{{ $t('pages.profile.input_field.confirm_password.label') }}</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-7">
+                                <div class="form-group has-danger mg-b-0">
+                                    <input :placeholder="$t('profile.input_field.new_password.placeholder')"
+                                           :class="{'is-error':((!vuelidate.profile.new_password.required || !vuelidate.profile.new_password.maxLength
+                                           || !vuelidate.profile.new_password.minLength) && vuelidate.$dirty) || (error_server.new_password && error_server.new_password.length)}"
+                                           class="form-control"
+                                           type="password"
+                                           v-model.trim="profile.new_password">
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="row text-end">
+                            <div class="col-lg-7 offset-lg-3 pt-3">
+                                <button class="btn background__green m__right--2 text-white" @click="submit">
+                                    {{ $t('buttons.add') }}
+                                </button>
+                                <button class="btn btn-secondary" @click="reset">
+                                    {{ $t('pages.profile.buttons.reset') }}
+                                </button>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
@@ -73,6 +263,7 @@ import {email, maxLength, minLength, required, requiredIf, sameAs} from "vuelida
 import {extension, fileSizeValidation, katakana} from "@/helpers/utils";
 import ProfileService from "@/services/ProfileService";
 import {ACTION_SET_ACTIVE_SIDEBAR} from "@/stores/common/actions";
+import {ACTION_SAVE_AUTH_USER} from "@/stores/auth/actions";
 
 export default {
     name: "Profile",
@@ -161,17 +352,48 @@ export default {
         async getProfile() {
             ProfileService.detail().then((res) => {
                 this.profile = Object.assign(this.profile, res.data)
-                console.log(this.profile)
-                this.avatar = this.profile.avatar
-                this.profile.avatar = null
-                this.profile.username = this.profile.username || ''
-                this.profile.phone = this.profile.phone || ''
-                this.profile.full_name = this.profile.full_name || ''
-                this.profile.katakana_name = this.profile.katakana_name || ''
-                this.oldProfile = res.data;
+                // this.avatar = this.profile.avatar
+                // this.profile.avatar = null
+                // this.profile.username = this.profile.username || ''
+                // this.profile.phone = this.profile.phone || ''
+                // this.profile.full_name = this.profile.full_name || ''
+                // this.profile.katakana_name = this.profile.katakana_name || ''
+                // this.oldProfile = res.data;
             }).catch((err) => {
                 this.$toast.error(err.data.message)
             })
+        },
+        submit() {
+            this.error_server = {}
+            this.vuelidate.$touch();
+            if (!this.vuelidate.$invalid && this.submitAvailable) {
+                let data = new FormData()
+                Object.keys(this.profile).map((e) => {
+                    if (e !== 'role' && e !== 'office')
+                        data.append(e, this.profile[e])
+                })
+                !this.profile.avatar && data.delete('avatar')
+                !this.profile.password && data.delete('password')
+                !this.profile.new_password && data.delete('new_password')
+                !this.profile.password_confirmation && data.delete('password_confirmation')
+                data.append('_method', 'PUT')
+                this.submitAvailable = false
+                ProfileService.update(this.profile.id, data)
+                        .then(() => {
+                            this.submitAvailable = true
+                            this.$toast.success(this.$t('profile.messages.update_success'))
+                            this.$store.dispatch(ACTION_SAVE_AUTH_USER)
+                        })
+                        .catch((err) => {
+                            this.submitAvailable = true
+                            this.error_server = err.data.errors
+                            // this.$toast.error(err.data.message)
+                        })
+            }
+        },
+        reset() {
+            this.profile = Object.assign({}, this.oldProfile);
+            this.$router.go(-1)
         },
     },
     created() {
